dd184b590173d8599e2d0c24f6eb43b49bbdecfa
chrgarcia19 <christian.garcia@valpo.edu>
2025-12-05 18:25:00 -0600

    Created a rough version of the mobile navigation menu

---
+++ static/navigation.js
@@ -0,0 +1,18 @@
+const hamburgerMenu = document.querySelector('.mobile-nav-button');
+const menu = document.querySelector('.mobile-menu-container');
+const menuIcon = document.getElementById("menu-icon")
+let menuOpen = false;
+
+hamburgerMenu.addEventListener("click", () => {
+    if (menuOpen){
+        menu.style.display = "flex";
+        menuIcon.classList.remove("bi-list");
+        menuIcon.classList.add("bi-x-lg");
+        menuOpen = false;
+    } else { 
+        menu.style.display = "none";
+        menuIcon.classList.remove("bi-x-lg");
+        menuIcon.classList.add("bi-list");
+        menuOpen = true;
+    }
+});
