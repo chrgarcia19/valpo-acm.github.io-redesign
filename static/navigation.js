0089193a0fd21d53ed9413ccf3a98ac93cfb2165
chrgarcia19 <christian.garcia@valpo.edu>
2025-12-10 19:29:47 -0600

    Finalized styling

--- static/navigation.js
+++ static/navigation.js
@@ -4,15 +4,15 @@ const menuIcon = document.getElementById("menu-icon")
 let menuOpen = false;
 
 hamburgerMenu.addEventListener("click", () => {
-    if (menuOpen){
-        menu.style.display = "flex";
-        menuIcon.classList.remove("bi-list");
-        menuIcon.classList.add("bi-x-lg");
-        menuOpen = false;
-    } else { 
+    if (!menuOpen){
         menu.style.display = "none";
         menuIcon.classList.remove("bi-x-lg");
         menuIcon.classList.add("bi-list");
         menuOpen = true;
+    } else { 
+        menu.style.display = "flex";
+        menuIcon.classList.remove("bi-list");
+        menuIcon.classList.add("bi-x-lg");
+        menuOpen = false;
     }
 });
