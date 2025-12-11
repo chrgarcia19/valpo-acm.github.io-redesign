ef27215135aa58482fe3485699abde357b065210
chrgarcia19 <christian.garcia@valpo.edu>
2025-12-10 19:46:39 -0600

    Added an animation to the nav bar menu on mobile

--- static/navigation.js
+++ static/navigation.js
@@ -5,14 +5,16 @@ let menuOpen = false;
 
 hamburgerMenu.addEventListener("click", () => {
     if (!menuOpen){
-        menu.style.display = "none";
-        menuIcon.classList.remove("bi-x-lg");
-        menuIcon.classList.add("bi-list");
-        menuOpen = true;
-    } else { 
+        menu.classList.add("open");
         menu.style.display = "flex";
         menuIcon.classList.remove("bi-list");
         menuIcon.classList.add("bi-x-lg");
+        menuOpen = true;
+    } else { 
+        menu.classList.remove("open");
+        menu.style.display = "none";
+        menuIcon.classList.remove("bi-x-lg");
+        menuIcon.classList.add("bi-list");
         menuOpen = false;
     }
 });
